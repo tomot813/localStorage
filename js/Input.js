bee01d81640448e0e057d7796b5cec3931012dd3
tomot813 <tomot813tom@gmail.com>
2024-12-04 01:21:16 +0300

    0.4v

--- js/Input.js
+++ js/Input.js
@@ -1,7 +1,9 @@
 function SetValue() {
+    alert("o")
     localStorage.removeItem("sharedData"); // Очистка предыдущкго значения
     localStorage.setItem("sharedData", document.getElementById("input").value); // Сохраняем в localStorage (у нее ограничение в 5 мб)
     document.getElementById("input").value = "";
+    alert("k")
 }
 
 document.getElementById("btn_set").addEventListener("click", SetValue);
